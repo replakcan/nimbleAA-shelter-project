a04be5e668747fe3e164b9a90f1f94d6af0bb58f
replakcan <alper.makcan@gmail.com>
2024-12-20 14:43:14 +0300

    Makes functions async

--- models/animal.js
+++ models/animal.js
@@ -2,14 +2,15 @@ const Injury = require("./injury");
 const uuid = require("uuid");
 
 module.exports = class Animal {
-  constructor(id = uuid.v4(), age, injuries = []) {
+  constructor(id = uuid.v4(), breed, age, injuries = []) {
     this.id = id;
+    this.breed = breed;
     this.age = age;
     this.injuries = injuries;
   }
 
-  static create({ id, age, injuries: injries }) {
-    const newAnimal = new Animal(id, age);
+  static create({ id, breed, age, injuries: injries }) {
+    const newAnimal = new Animal(id, breed, age);
 
     newAnimal.injuries = injries?.map((injury) => Injury.create(injury));
 
