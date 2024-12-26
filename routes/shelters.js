0a5d2ec1ed6341b415c6ea0145603fa56948c6a7
replakcan <alper.makcan@gmail.com>
2024-12-26 21:43:24 +0300

    Updates manager and shelter views

--- routes/shelters.js
+++ routes/shelters.js
@@ -9,10 +9,8 @@ router.get("/", async (req, res) => {
 
 //abstraction-leak
 router.get("/:shelterId", async (req, res) => {
-  const shelter = await shelterDatabase.findBy(
-    "_id",
-    req.params.shelterId
-  );
+  const { shelterId } = req.params;
+  const shelter = await shelterDatabase.find(shelterId);
 
   if (!shelter)
     return res.status(404).send("There is no shelter with given id");
@@ -20,11 +18,19 @@ router.get("/:shelterId", async (req, res) => {
   res.render("shelter", { shelter });
 });
 
+router.get("/:shelterId/animal-list", async (req, res) => {
+  const { shelterId } = req.params;
+  const shelter = await shelterDatabase.find(shelterId);
+
+  const animalList = shelter.animalList;
+
+  res.render("manager-animal-list", { animalList });
+});
+
 router.post("/", async (req, res) => {
   const shelter = await shelterDatabase.insert(req.body);
 
   res.send(shelter);
 });
-
 
 module.exports = router;
